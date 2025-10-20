a55dbcc58719a0405666f6c1c5ef8778a3385ce9
Joel Martínez Suárez <jobichi@localhost.localdomain>
2025-10-20 17:28:26 +0200

    Modificaciones para comprobar el correcto funcionamiento del servicio ari

--- services/ari-service/main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import asyncio
-from ari_client import ARIClient
-
-if __name__ == "__main__":
-    try:
-        asyncio.run(ARIClient().run())
-    except KeyboardInterrupt:
-        pass
