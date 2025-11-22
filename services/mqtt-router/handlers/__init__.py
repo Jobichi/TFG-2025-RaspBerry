1253bf987b1bd07caa9299dbc957f6c1a8689836
Joel Martínez Suárez <jobichi@localhost.localdomain>
2025-11-22 17:43:59 +0100

    Ajustes

--- services/mqtt-router/handlers/__init__.py
+++ services/mqtt-router/handlers/__init__.py
@@ -2,6 +2,7 @@ from .announce import handle as announce
 from .update import handle as update
 from .alert import handle as alert
 from .response import handle as response
-from .set import handle as set_handler
-from .select import handle as select_handler
+from .esp_set import handle as esp_set
+from .esp_get import handle as esp_get
+from .db_select import handle as db_select
 from .notify import handle as notify
